67e5abb83db267c6582c87b594996cd922a0ab07
codewitch-honey-crisis <gaztoast@gmail.com>
2023-03-07 00:35:15 -0800

    first commit

---
+++ htcw_uix/include/uix.hpp
@@ -0,0 +1,9 @@
+#ifndef HTCW_UIX_HPP
+#define HTCW_UIX_HPP
+#include "uix_core.hpp"
+#include "uix_timer.hpp"
+#include "uix_screen.hpp"
+#include "uix_label.hpp"
+#include "uix_push_button.hpp"
+#include "uix_canvas.hpp"
+#endif
